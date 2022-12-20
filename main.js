b43b1108834652186d8d4be5bb864f3557e3dff5
AKBagheL <ak9664773@gmail.com>
2022-12-20 11:37:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let total=0;
+
+document.querySelector('.positive').addEventListener('click',increase);
+document.querySelector('.negative').addEventListener('click',decrease);
+document.querySelector('.reset').addEventListener('click',reset);
+
+function increase(){
+    let input=document.getElementById('num').value;
+    total+=Number(input);
+    console.log(total)
+    document.querySelector('.counter').innerText=total;
+}
+
+function decrease(){
+    let input=document.getElementById('num').value;
+    total-=Number(input);
+    document.querySelector('.counter').innerText=total;
+}
+
+function reset(){
+    document.querySelector('.counter').innerText=0;
+    document.getElementById('num').value="";
+}
